1c1ab7389422edf2fc8e31b2e2dce0d5d0cc7413
Gexeg <psiondnd@yandex.ru>
2018-09-14 22:45:02 +0500

    Сортировка вставками

--- ls20.py
+++ ls20.py
@@ -2,14 +2,14 @@ def sorting(step,data):
     for index in range(1, len(data)):
         if index % step == 0:
             currentvalue = data[index]
-            position = index
-            while position > 0 and data[position - 1] > currentvalue:
-                data[position] = data[position - 1]
-                position = position - 1
-            data[position] = currentvalue
+            while index > 0 and data[index - 1] > currentvalue:
+                data[index] = data[index - 1]
+                index = index - 1
+            data[index] = currentvalue
     return data
 
 
 a = [7,6,5,4,3,2,1]
 
 print(sorting(3,a))
+
