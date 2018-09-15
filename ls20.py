0734b29cad475ad2e3194b3f6889522617404abd
Gexeg <psiondnd@yandex.ru>
2018-09-15 12:43:30 +0500

    Сортировка вставками

--- ls20.py
+++ ls20.py
@@ -1,15 +1,17 @@
-def sorting(step,data):
+def sorting2(step, data):
+    """  Алгоритм не сортирует список полностью, он лишь 1 раз проходит по списку с заданным шагом"""
     for index in range(1, len(data)):
         if index % step == 0:
             currentvalue = data[index]
-            while index > 0 and data[index - 1] > currentvalue:
-                data[index] = data[index - 1]
-                index = index - 1
-            data[index] = currentvalue
+            position = index
+            while position > 0 and data[position - step] > currentvalue:
+                data[position] = data[position - step]
+                position = position - step
+            data[position] = currentvalue
     return data
 
 
-a = [7,6,5,4,3,2,1]
+a = [7, 6, 5, 4, 3, 2, 1]
 
-print(sorting(3,a))
+print(sorting2(3, a))
 
