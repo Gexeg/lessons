2287174875ca1b352aa9a44b79af1007366af365
Gexeg <psiondnd@yandex.ru>
2018-09-15 19:03:45 +0500

    Сортировка вставками

--- ls20.py
+++ ls20.py
@@ -1,19 +1,24 @@
 def sorting3(step, data):
     """  Алгоритм проходит по всем элементам массива"""
-    circle = 1
-    while circle <= step:
+    circle = 0
+    while circle < step:
         for index in range(circle, len(data), step):
-            currentvalue = data[index]
+            current_value = data[index]
             position = index
-            while position > 0 and data[position - step] > currentvalue:
+            print(circle, ' circ')
+            print(position, ' pos')
+            print(current_value,  ' cur')
+            while position - step  >= 0 and data[position - step] > current_value:
                 data[position] = data[position - step]
                 position = position - step
-            data[position] = currentvalue
+            data[position] = current_value
+            print(data)
+            print()
         circle +=1
     return data
 
 
-a = [7, 6, 5, 4, 3, 2, 1]
+a = [1, 6, 5, 7, 3, 2, 4]
 
 print(sorting3(3, a))
 
