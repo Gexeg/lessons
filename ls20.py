07c6eb271c128deddb6013078e620a7e79c20680
Gexeg <psiondnd@yandex.ru>
2018-09-15 16:39:55 +0500

    Сортировка вставками

--- ls20.py
+++ ls20.py
@@ -1,17 +1,19 @@
-def sorting2(step, data):
-    """  Алгоритм не сортирует список полностью, он лишь 1 раз проходит по списку с заданным шагом"""
-    for index in range(1, len(data)):
-        if index % step == 0:
+def sorting3(step, data):
+    """  Алгоритм проходит по всем элементам массива"""
+    circle = 1
+    while circle <= step:
+        for index in range(circle, len(data), step):
             currentvalue = data[index]
             position = index
             while position > 0 and data[position - step] > currentvalue:
                 data[position] = data[position - step]
                 position = position - step
             data[position] = currentvalue
+        circle +=1
     return data
 
 
 a = [7, 6, 5, 4, 3, 2, 1]
 
-print(sorting2(3, a))
+print(sorting3(3, a))
 
