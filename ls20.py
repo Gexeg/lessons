68beb82ba89445ca51a6ff1b634887d63bae0ac5
Gexeg <psiondnd@yandex.ru>
2018-09-18 20:27:24 +0500

    Сортировка Шелла

--- ls20.py
+++ ls20.py
@@ -1,24 +1,32 @@
-def sorting3(step, data):
+import random
+
+def shell_sort(data):
     """  Алгоритм проходит по всем элементам массива"""
-    circle = 0
-    while circle < step:
-        for index in range(circle, len(data), step):
+    print('Сгенерированный массив')
+    print(data)
+    print()
+    print('Шаги сортировки')
+    step = 1
+    while (3 * step) + 1 < len(data):
+        step = (3 * step) +1
+    while step > 0:
+        for index in range(1, len(data), step):
             current_value = data[index]
             position = index
-            print(circle, ' circ')
-            print(position, ' pos')
-            print(current_value,  ' cur')
             while position - step  >= 0 and data[position - step] > current_value:
                 data[position] = data[position - step]
                 position = position - step
             data[position] = current_value
-            print(data)
-            print()
-        circle +=1
+        print(step)
+        step = step // 3
+    print()
+    print('Отсортированный массив')
     return data
 
+a = []
 
-a = [1, 6, 5, 7, 3, 2, 4]
+for i in range(random.randint(1,20)):
+    a.append(random.randint(0,100))
 
-print(sorting3(3, a))
+print(shell_sort(a))
 
