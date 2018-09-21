62900a065852bc8da656f873304e54071a56df2e
Gexeg <psiondnd@yandex.ru>
2018-09-21 19:19:40 +0500

    Разбиение

--- ls21.py
+++ ls21.py
@@ -1,26 +1,29 @@
 import random
 
 def array_s(list_in):
+    if len(list_in) == 1:
+        return False
     result = list_in
-    list_avg = int(sum(result) / len(result))
+    list_avg = sum(result) / len(result)
     start_i = 0
     end_i = len(result) - 1
-    while start_i != end_i - 1:
-        if start_i == end_i or start_i > end_i:
+    while True:
+        if start_i == end_i - 1:
             break
         if result[start_i] <= list_avg:
             start_i += 1
-        if result[end_i] >= list_avg:
+            continue
+        if result[end_i] > list_avg:
             end_i -= 1
-        result[start_i], result [end_i] = result[end_i], result[start_i]
-
-    return result, list_avg
+        result[start_i], result[end_i] = result[end_i], result[start_i]
+        print(result, start_i, end_i)
+    return result, list_avg, start_i, end_i
 
 
 a = []
 
 for i in range(random.randint(1,20)):
-    a.append(random.randint(0,100))
+    a.append(random.randint(1,1))
 
 print(a)
 print(array_s(a))
