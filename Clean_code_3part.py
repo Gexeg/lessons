a81df045f3c79de3d5d7990b03c1930994cdce20
Gexeg <psiondnd@yandex.ru>
2018-11-18 21:48:01 +0500

    for_blog

--- Clean_code_3part.py
+++ Clean_code_3part.py
@@ -1,32 +1,15 @@
 from random import randint
 import os
 
-class FilesStorage:
-    def __init__(self, absolute_path):
-        """Структура данных, хранящая в себе информацию о пути до директории содержащей файлы и
-        файлах, помещенных в эту директорию"""
-        self.absolute_path = absolute_path
-        self.file_names_with_path = []
-
-
 class FileOperations:
     """Объект, взаимодействующий со структурой данных"""
     def __init__(self, files_storage_structure):
         self.files_storage = files_storage_structure
 
     def fill_storage(self, number_of_files):
+        """Метод, наполняющий хранилище файлами, заполненными случайными числами"""
         for file in range(number_of_files):
             self.create_file()
-
-    def create_file(self):
-        file_name = len(self.files_storage.file_names_with_path)
-        with open(self.files_storage.absolute_path + str(file_name), 'wt+') as new_file:
-            for line in range(3):
-                new_file.write(str(randint(0, 100)) + '\n')
-            self.insert_file_in_storage(file_name)
-
-    def insert_file_in_storage(self, file_name):
-        self.files_storage.file_names_with_path.append(str(self.files_storage.absolute_path) + str(file_name))
 
     def summ_from_random_files(self):
         """метод, выбирающий 2 случайных файла из списка доступных. Перед выполнением с помощью
@@ -44,8 +27,27 @@ class FileOperations:
                         sum = sum + int(line)
                     return sum
 
+    def create_file(self):
+        file_name = len(self.files_storage.file_names_with_path)
+        with open(self.files_storage.absolute_path + str(file_name), 'wt+') as new_file:
+            for line in range(3):
+                new_file.write(str(randint(0, 100)) + '\n')
+            self.insert_file_in_storage(file_name)
+
+    def insert_file_in_storage(self, file_name):
+        self.files_storage.file_names_with_path.append(str(self.files_storage.absolute_path) + str(file_name))
+
+
     def choose_random_file(self):
         return self.files_storage.file_names_with_path[randint(0, len(self.files_storage.file_names_with_path))]
+
+
+class FilesStorage:
+    def __init__(self, absolute_path):
+        """Структура данных, хранящая в себе информацию о пути до директории содержащей файлы и
+        файлах, помещенных в эту директорию"""
+        self.absolute_path = absolute_path
+        self.file_names_with_path = []
 
 
 class Storage_exceptions:
