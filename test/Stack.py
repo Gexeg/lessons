049b0c862724f22549d0d9bf484d9bcbf8aa8ccb
Gexeg <psiondnd@yandex.ru>
2018-12-18 22:03:00 +0500

    first_version

--- test/Stack.py
+++ test/Stack.py
@@ -1,3 +1,5 @@
+import unittest
+
 class Stack:
     def __init__(self):
         self.stack = []
@@ -17,3 +19,33 @@ class Stack:
         if len(self.stack) == 0:
             return None
         return self.stack[0]
+
+class ls2_test(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_size_and_peek(self):
+        test_stack = Stack()
+        test_stack.push(123)
+        test_stack.push(456)
+        self.assertEqual(test_stack.peek(), 456)
+        self.assertEqual(test_stack.size(), 2)
+
+    def test_pop(self):
+        test_stack = Stack()
+        test_stack.push(123)
+        test_stack.push(456)
+        self.assertEqual(test_stack.pop(), 456)
+
+    def test_pop_empty_stack(self):
+        test_stack = Stack()
+        self.assertEqual(test_stack.pop(), None)
+
+    def test_peek(self):
+        test_stack = Stack()
+        self.assertEqual(test_stack.peek(), None)
+        self.assertEqual(test_stack.size(), 0)
+
+    def tearDown(self):
+        pass
