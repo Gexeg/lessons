b521cad98b4e7aaecbcc6315e774c47efd45d1b5
Gexeg <psiondnd@yandex.ru>
2018-12-18 18:58:45 +0500

    first_version

---
+++ test/Stack.py
@@ -0,0 +1,19 @@
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def size(self):
+        return len(self.stack)
+
+    def pop(self):
+        if len(self.stack) == 0:
+            return None
+        return self.stack.pop(0)
+
+    def push(self, value):
+        return self.stack.insert(0, value)
+
+    def peek(self):
+        if len(self.stack) == 0:
+            return None
+        return self.stack[0]
