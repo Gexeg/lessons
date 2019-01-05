21b8b99e68f7d7873bd9330593972cccdc83f8d2
Gexeg <psiondnd@yandex.ru>
2019-01-05 18:38:36 +0500

    second_version

--- test/Deque.py
+++ test/Deque.py
@@ -1,3 +1,6 @@
+import unittest
+
+
 class Deque:
     def __init__(self):
         self.queue = []
@@ -23,3 +26,48 @@ class Deque:
     def size(self):
         return len(self.queue)
 
+
+class ls2_test(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_dequeue(self):
+        a = Deque()
+        a.addFront('c')
+        a.addFront('D')
+        a.addFront('B')
+        self.assertEqual(a.removeTail(), 'c')
+        self.assertEqual(a.removeFront(), 'B')
+
+    def test_addTail(self):
+        a = Deque()
+        a.addFront('c')
+        a.addFront('B')
+        a.addTail('B')
+        self.assertEqual(a.removeTail(), 'B')
+        self.assertEqual(a.removeFront(), 'B')
+        self.assertEqual(a.size(), 1)
+
+    def test_addTail_2(self):
+        a = Deque()
+        a.addTail('c')
+        a.addTail('B')
+        a.addTail('B')
+        self.assertEqual(a.removeTail(), 'B')
+        self.assertEqual(a.removeFront(), 'c')
+
+    def test_remove_empty(self):
+        a = Deque()
+        a.addTail('c')
+        self.assertEqual(a.removeFront(), 'c')
+        self.assertEqual(a.removeFront(), None)
+        self.assertEqual(a.removeTail(), None)
+
+    def test_remove_empty_2(self):
+        a = Deque()
+        self.assertEqual(a.removeFront(), None)
+        self.assertEqual(a.removeTail(), None)
+
+    def tearDown(self):
+        pass
