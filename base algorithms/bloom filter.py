ee4f9389e4c1caee5664b7912426baea8e72121f
Gexeg <psiondnd@yandex.ru>
2019-02-02 22:10:41 +0500

    first_version

--- base algorithms/bloom filter.py
+++ base algorithms/bloom filter.py
@@ -26,3 +26,19 @@ class BloomFilter:
 
     def is_value(self, str1):
         return self.bitarray[self.hash1(str1)] and self.bitarray[self.hash2(str1)]
+
+
+bloom_filter = BloomFilter(32)
+
+strings = ['0123456789', '1234567890', '2345678901','3456789012', '4567890123','5678901234',
+           '6789012345','7890123456','8901234567','9012345678']
+
+for i in strings:
+    bloom_filter.add(i)
+
+false_counter = 0
+for i in strings:
+    if bloom_filter.is_value(i) is False:
+        false_counter +=1
+
+print(false_counter)
