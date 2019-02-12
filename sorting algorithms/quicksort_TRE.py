b720dd8939218e47517cd637af57f632e3a00db4
Gexeg <psiondnd@yandex.ru>
2019-02-12 21:51:53 +0500

    sorting algorithms

--- sorting algorithms/quicksort_TRE.py
+++ sorting algorithms/quicksort_TRE.py
@@ -14,22 +14,49 @@ def separ(array, start, end):
 
 
 def quicksort(array, start, end):
-    while start < end:
-        pos = separ(array, start, end)
-        if pos - start < end - pos:
-            quicksort(array, start, pos - 1)
-            start = pos + 1
+    if start < end:
+        position = separ(array, start, end)
+        if position - start > end - position:
+            stack = []
+            stack.append(start)
+            stack.append(position - 1)
+            while len(stack) > 0:
+                wh_end = stack.pop()
+                wh_start = stack.pop()
+                pos = separ(array, wh_start, wh_end)
+                if pos - 1 > wh_start:
+                    stack.append(wh_start)
+                    stack.append(pos-1)
+                if pos + 1 < wh_end:
+                    stack.append(pos + 1)
+                    stack.append(wh_end)
+            return quicksort(array, position + 1, end)
         else:
-            quicksort(array, pos + 1, end)
-            end = pos - 1
+            stack = []
+            stack.append(position + 1)
+            stack.append(end)
+            while len(stack) > 0:
+                wh_end = stack.pop()
+                wh_start = stack.pop()
+                pos = separ(array, wh_start, wh_end)
+                if pos - 1 > wh_start:
+                    stack.append(wh_start)
+                    stack.append(pos - 1)
+                if pos + 1 < wh_end:
+                    stack.append(pos + 1)
+                    stack.append(wh_end)
+            return quicksort(array, position + 1, end)
 
+arr = [1,10,5,9,0, 1293,1023,0,12]
 
-big_arr_quicksort = [random.random() for i in range(10000)]
-big_arr_p_sort = [random.random() for i in range(10000)]
+quicksort(arr, 0, len(arr) - 1)
+print(arr)
+
+big_arr_quicksort = [random.random() for i in range(1000)]
+big_arr_p_sort = [random.random() for i in range(1000)]
 
 t1 = timeit.Timer(lambda: quicksort(big_arr_quicksort, 0, len(big_arr_quicksort)-1)).timeit(number=1)
 t3 = timeit.Timer(lambda: big_arr_p_sort.sort()).timeit(number=1)
 
 print('Quicksort time',t1)
 print('Standart sort time',t3)
-
