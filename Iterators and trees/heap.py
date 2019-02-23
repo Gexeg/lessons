ecad91beb365109c2d82f648a008904ae98d2f7f
Gexeg <psiondnd@yandex.ru>
2019-02-23 11:31:29 +0500

    iterators and trees

--- Iterators and trees/heap.py
+++ Iterators and trees/heap.py
@@ -1,52 +1,81 @@
-import ctypes
-
+import random, timeit
 
 class Heap:
-    def __init__(self, value):
-        self.count = 1
-        self.capacity = 15
-        self.array = (self.capacity * ctypes.py_object)()
-        for i in range(self.capacity):
-            self.array[i] = None
-        self.array[0] = value
 
-    def swap(self, i, j):
-        self.array[i], self.array[j] = self.array[j], self.array[i]
-        return
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.last_node_index = 0
+        self.storage = [None] * capacity
 
     def add_node(self, value):
-        """Добавление нового узла. Родитель больше ребенка"""
-        if self.count == self.capacity:
+        if self.last_node_index == self.capacity:
             return None
-        self.array[self.count] = value
-        self.count += 1
-        current_ind = self.count - 1
+        new_node = Node(value)
+        current_ind = self.last_node_index
+        self.storage[self.last_node_index] = new_node
+        self.last_node_index += 1
         while current_ind != 0:
-            if self.array[current_ind] < self.array[(current_ind-1)//2]:
-                return
-            if self.array[current_ind] > self.array[(current_ind-1)//2]:
-                self.swap(current_ind, (current_ind - 1) // 2)
-            current_ind = (current_ind-1)//2
-        return
+            new_node_value = self.storage[current_ind].value
+            parent_node_value = self.storage[(current_ind - 1) // 2].value
+            parent_node_index = (current_ind - 1) // 2
+            if new_node_value < parent_node_value:
+                break
+            if new_node_value > parent_node_value:
+                self.swap(current_ind, parent_node_index)
+            current_ind = parent_node_index
 
     def pop(self):
         """Получение корня"""
-        pop = self.array[0]
-        self.array[0] = self.array[self.count-1]
-        self.array[self.count-1] = None
+        if self.storage[0]:
+            pop = self.storage[0]
+        else:
+            return None
+        self.storage[0] = self.storage[self.last_node_index-1]
+        self.storage[self.last_node_index-1] = None
+        self.last_node_index -= 1
         current_ind = 0
         while True:
-            if current_ind * 2 + 1 < self.capacity and self.array[current_ind] < self.array[current_ind * 2 + 1]:
-              self.swap(current_ind, current_ind * 2 + 1)
-              current_ind = current_ind * 2 + 1
-            elif current_ind * 2 + 2 < self.capacity and self.array[current_ind] < self.array[current_ind * 2 + 2]:
-                self.swap(current_ind, current_ind * 2 + 2)
-                current_ind = current_ind * 2 + 2
+            r_child_index = current_ind * 2 + 2 if current_ind * 2 + 2 < len(self.storage) - 1 else None
+            if r_child_index:
+                r_child_value = self.storage[r_child_index].value if self.storage[r_child_index] else None
+            else:
+                r_child_value = None
+            l_child_index = current_ind * 2 + 1 if current_ind * 2 + 1 < len(self.storage) - 1 else None
+            if l_child_index:
+                l_child_value = self.storage[l_child_index].value if self.storage[l_child_index] else None
+            else:
+                l_child_value = None
+            if r_child_value and l_child_value:
+               if r_child_value > l_child_value:
+                   max_child_ind = r_child_index
+                   max_child_value = r_child_value
+               else:
+                   max_child_ind = l_child_index
+                   max_child_value = l_child_value
+            elif r_child_value:
+                max_child_ind = r_child_index
+                max_child_value = r_child_value
+            elif l_child_value:
+                max_child_ind = l_child_index
+                max_child_value = l_child_value
+            else:
+                return pop
+            if self.storage[current_ind].value < max_child_value:
+                self.swap(current_ind, max_child_ind)
+                current_ind = max_child_ind
             else:
                 return pop
 
+    def swap(self, i, j):
+        self.storage[i], self.storage[j] = self.storage[j], self.storage[i]
+        return
 
-mas_tree = Heap(50)
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+
+mas_tree = Heap(5)
 mas_tree.add_node(25)
 mas_tree.add_node(75)
 mas_tree.add_node(84)
@@ -63,13 +92,15 @@ mas_tree.add_node(15)
 mas_tree.add_node(9)
 
 for i in range(mas_tree.capacity):
-    print(str(i)+'   '+str(mas_tree.array[i]))
+    value = mas_tree.storage[i].value if mas_tree.storage[i] else 'None'
+    print(str(i)+'   '+str(value))
 
 print()
 
-print(mas_tree.pop())
+print(mas_tree.pop().value)
 
 print()
 
 for i in range(mas_tree.capacity):
-    print(str(i)+'   '+str(mas_tree.array[i]))
+    value = mas_tree.storage[i].value if mas_tree.storage[i] else 'None'
+    print(str(i)+'   '+str(value))
