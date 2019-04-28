8bbf585c55ca16e62fb6e22e16f91b1027893946
Gexeg <psiondnd@yandex.ru>
2019-04-28 12:19:26 +0500

    Heap

--- Iterators and trees/heap.py
+++ Iterators and trees/heap.py
@@ -1,7 +1,11 @@
+import unittest
+from random import choice
+
+
 class Heap:
 
     def __init__(self):
-        self.HeapArray = []  # хранит неотрицательные числа-ключи
+        self.HeapArray = []# хранит неотрицательные числа-ключи
 
     def MakeHeap(self, array):
         new_tree_capacity = 0
@@ -35,7 +39,7 @@ class Heap:
         """Получение корня"""
         if len(self.HeapArray) == 0 or self.HeapArray[0] is None:
             return -1
-        none_ind = next((i - 1 for i, x in enumerate(self.HeapArray) if x is None), len(self.HeapArray) - 1)
+        none_ind = next((i - 1 for i, x in enumerate(self.HeapArray) if x is None), len(self.HeapArray)-1)
         pop = self.HeapArray[0]
         self.HeapArray[0] = self.HeapArray[none_ind]
         self.HeapArray[none_ind] = None
@@ -73,4 +77,114 @@ class Heap:
 
     def swap(self, i, j):
         self.HeapArray[i], self.HeapArray[j] = self.HeapArray[j], self.HeapArray[i]
+
+
+
+class ls2_test(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_make_heap(self):
+        array = [10, 15, 6, 22, 34, 86]
+        new_heap = Heap()
+        new_heap.MakeHeap(array)
+        etalon_array = [86, 22, 34, 10, 15, 6, None]
+        self.assertEqual(new_heap.HeapArray, etalon_array)
+
+    def test_empty_heap_add(self):
+        new_heap = Heap()
+        self.assertEqual(new_heap.Add(15), False)
+
+    def test_make_heap_2(self):
+        array = [10, 15, 37, 22, 56, 86]
+        new_heap = Heap()
+        new_heap.MakeHeap(array)
+        array2 = [10, 15, 6, 22, 34, 86]
+        new_heap.MakeHeap(array2)
+        etalon_array = [86, 22, 34, 10, 15, 6, None]
+        self.assertEqual(new_heap.HeapArray, etalon_array)
+
+
+    def test_GetMax(self):
+        array = [10, 15, 37, 22, 56, 86]
+        new_heap = Heap()
+        new_heap.MakeHeap(array)
+        roots = [86, 56, 37, 22, 15, 10]
+        for ind in range(6):
+            self.assertEqual(new_heap.GetMax(), roots[ind])
+        empty_array = [None] * 7
+        self.assertEqual(len(new_heap.HeapArray), 7)
+        self.assertEqual(new_heap.HeapArray, empty_array)
+
+    def test_GetMax_make_new(self):
+        array = [10, 15, 6, 22, 34, 86]
+        new_heap = Heap()
+        new_heap.MakeHeap(array)
+        for key in array:
+            self.assertEqual(new_heap.GetMax() is not None, True)
+            self.assertEqual(len(new_heap.HeapArray), 7)
+        new_heap.MakeHeap(array)
+        self.assertEqual(len(new_heap.HeapArray), 7)
+        etalon_array = [86, 22, 34, 10, 15, 6, None]
+        self.assertEqual(new_heap.HeapArray, etalon_array)
+
+    def test_GetMax_make_new2(self):
+        array = [10, 15, 6, 22, 34, 86]
+        new_heap = Heap()
+        new_heap.MakeHeap(array)
+        for key in range(3):
+            self.assertEqual(new_heap.GetMax() is not None, True)
+        new_heap.MakeHeap(array)
+        self.assertEqual(len(new_heap.HeapArray), 7)
+        etalon_array = [86, 22, 34, 10, 15, 6, None]
+        self.assertEqual(new_heap.HeapArray, etalon_array)
+
+    def test_GetMax_make_new3(self):
+        array = [10]
+        new_heap = Heap()
+        new_heap.MakeHeap(array)
+        self.assertEqual(new_heap.GetMax(), 10)
+        self.assertEqual(new_heap.HeapArray, [None, None, None])
+        new_heap.MakeHeap(array)
+        self.assertEqual(len(new_heap.HeapArray), 3)
+        etalon_array = [10, None, None]
+        self.assertEqual(new_heap.HeapArray, etalon_array)
+
+    def test_GetMax_Add(self):
+        array = [10, 15, 6, 22, 34, 86]
+        new_heap = Heap()
+        new_heap.MakeHeap(array)
+        for key in range(8):
+            self.assertEqual(new_heap.GetMax() is not None, True)
+        for i in range(6):
+            new_heap.Add(array[i])
+        etalon_array = [86, 22, 34, 10, 15, 6, None]
+        self.assertEqual(new_heap.HeapArray, etalon_array)
+
+    def test_GetMax_Add2(self):
+        array = [10, 15, 6, 22, 34, 86, 5]
+        new_heap = Heap()
+        new_heap.MakeHeap(array)
+        for i in range(6):
+            self.assertEqual(new_heap.Add(array[i]), False)
+        etalon_array = [86, 22, 34, 10, 15, 6, 5]
+        self.assertEqual(new_heap.HeapArray, etalon_array)
+
+
+    def test_GetMax_Add3(self):
+        array = [10, 15, 6, 22, 34, 86]
+        new_heap = Heap()
+        new_heap.MakeHeap(array)
+        for i in range(82):
+            self.assertEqual(new_heap.GetMax() is not None, True)
+            self.assertEqual(len(new_heap.HeapArray), 7)
+            self.assertEqual(new_heap.Add(choice(array)), True)
+        self.assertEqual(len(new_heap.HeapArray), 7)
+
+    def tearDown(self):
+        pass
+
+
+unittest.main()
 
