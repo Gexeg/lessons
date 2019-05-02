219accb98d71d64eaf74f83c785d9747935fc14a
Gexeg <psiondnd@yandex.ru>
2019-05-02 09:06:29 +0500

    even_forest

--- Iterators and trees/even_forest.py
+++ Iterators and trees/even_forest.py
@@ -1,3 +1,5 @@
+import unittest
+
 class SimpleTreeNode:
 
     def __init__(self, val, parent):
@@ -68,6 +70,8 @@ class SimpleTree:
         return leaf_counter
 
     def EvenTrees(self):
+        """Метод разбивает дерево на максимальное количество минимальных четных деревьев
+        на выходе будет список объектов SimpleTreeNode, между которыми нужно разорвать связь"""
         result = []
         def count_DFS(root):
             nonlocal result
@@ -85,3 +89,65 @@ class SimpleTree:
             return nodes
         count_DFS(self.Root)
         return result
+
+
+
+class ls2_test(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_lesson_example(self):
+        a_tree = SimpleTree(None)
+        root_node = SimpleTreeNode(1, None)
+        a_tree.Root = root_node
+
+        two = SimpleTreeNode(2, None)
+        a_tree.AddChild(a_tree.Root, two)
+        five = SimpleTreeNode(5, None)
+        a_tree.AddChild(two, five)
+        seven = SimpleTreeNode(7, None)
+        a_tree.AddChild(two, seven)
+
+        three = SimpleTreeNode(3, None)
+        a_tree.AddChild(a_tree.Root, three)
+        four = SimpleTreeNode(4, None)
+        a_tree.AddChild(three, four)
+
+        six = SimpleTreeNode(6, None)
+        a_tree.AddChild(a_tree.Root, six)
+        eight = SimpleTreeNode(8, None)
+        a_tree.AddChild(six, eight)
+        nine = SimpleTreeNode(9, None)
+        a_tree.AddChild(eight, nine)
+        ten = SimpleTreeNode(10, None)
+        a_tree.AddChild(eight, ten)
+        self.assertEqual(a_tree.EvenTrees(), [a_tree.Root, three, a_tree.Root, six])
+
+    def test_another_example(self):
+        a_tree = SimpleTree(None)
+        root_node = SimpleTreeNode(1, None)
+        a_tree.Root = root_node
+
+        nine = SimpleTreeNode(9, None)
+        a_tree.AddChild(a_tree.Root, nine)
+
+        two = SimpleTreeNode(2, None)
+        a_tree.AddChild(a_tree.Root, two)
+        four = SimpleTreeNode(4, None)
+        a_tree.AddChild(two, four)
+
+        three = SimpleTreeNode(3, None)
+        a_tree.AddChild(a_tree.Root, three)
+        five = SimpleTreeNode(5, None)
+        a_tree.AddChild(three, five)
+        six = SimpleTreeNode(6, None)
+        a_tree.AddChild(five, six)
+
+        self.assertEqual(a_tree.EvenTrees(), [a_tree.Root, two, three, five])
+
+    def tearDown(self):
+        pass
+
+
+unittest.main()
