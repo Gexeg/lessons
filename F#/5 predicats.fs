fad36770e8e156b6e1704359018a7c9984646cf5
Gexeg <psiondnd@yandex.ru>
2019-05-28 23:12:06 +0500

    predicats

--- F#/5 predicats.fs
+++ F#/5 predicats.fs
@@ -3,6 +3,6 @@ let notDivisible (n,m) = m % n = 0
 
 // 16.2
 let  prime x =
-  seq { for i = 2 to x/2 do if x % i = 0 then yeld i}
+  seq { for i = 2 to x/2 do if x % i = 0 then yield i}
   |> Seq.isEmpty
 
