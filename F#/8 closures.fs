d0d6b8801b2a9a4ceeb2810c95f39ed50b08e3d9
Gexeg <psiondnd@yandex.ru>
2019-06-10 23:33:32 +0500

    closures

--- F#/8 closures.fs
+++ F#/8 closures.fs
@@ -1,5 +1,5 @@
 // 21 closures
-let curry in_function arg1 arg2 = in_function arg1 arg2
+let curry in_function = function arg1 -> function arg2 -> in_function(arg1,arg2)
 
 let uncurry in_function = function (x,y) -> (in_function x) y
 
