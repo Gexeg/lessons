943d5f7d250fb8c1d1a9594522e79d7b6261c847
Gexeg <psiondnd@yandex.ru>
2019-06-12 19:25:01 +0500

    tags

--- F#/11 tags.fs
+++ F#/11 tags.fs
@@ -1,8 +1,14 @@
-type TimeOfDay = { hours: int; minutes: int; f: string }
+//tags
+type F = 
+  | AM
+  | PM
+
+type TimeOfDay = { hours : int; minutes : int; f: F }
 
 let (.>.) x y = match (x,y) with
-  | (x,y) when x.f="PM" && y.f="AM" -> true
-  | (x,y) when x.f="AM" && y.f="PM" -> false
+  | (x,y) when x.f=PM && y.f=AM -> true
+  | (x,y) when x.f=AM && y.f=PM -> false
   | (x,y) when x.hours > y.hours -> true
-  | (x,y) when x.hours = y.hours && x.minutes > y.hours -> true
+  | (x,y) when x.minutes > y.hours -> true
   | _ -> false
+
