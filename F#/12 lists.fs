a7f743a582cf965939301e605254e2c782d277ce
Gexeg <psiondnd@yandex.ru>
2019-06-15 20:47:54 +0500

    lists

--- F#/12 lists.fs
+++ F#/12 lists.fs
@@ -1,23 +1,25 @@
 // 34.1
-let upto n = 
-  let rec build_list (q,k,n_list) = match (q,k,n_list) with
+let rec upto n = 
+  let rec build_list = function
     | (q,k,n_list) when k = 0 -> n_list 
     | (q,k,n_list) -> build_list(q,k-1,k::n_list)
     | (_,_,_) -> []
   build_list (n, n, [])
 
 // 34.2
-let dnto n = 
-  let rec build_list (q,k,n_list) = match (q,k,n_list) with
+let rec dnto n = 
+  let rec build_list = function
     | (q,k,n_list) when k = q + 1 -> n_list 
     | (q,k,n_list) -> build_list(q,k+1,k::n_list)
     | (_,_,_) -> []
   build_list (n, 1, [])
 
+
 // 34.3
-let even n = 
-    let rec build_list (q,k,(n_list: 'T list)) = match (q,k,n_list) with
-        | (q,k,n_list) when n_list.Length = q -> n_list 
-        | (q,k,n_list) -> build_list(q,k+2,k::n_list)
-        | (_,_,_) -> []
-    build_list (n, 2, [])
+let rec even n = 
+    let rec build_list = function
+        | (q,k,n_list, j) when j = q -> n_list 
+        | (q,k,n_list, j) -> build_list(q,k+2,k::n_list, j+1)
+        | (_,_,_,_) -> []
+    build_list (n, 2, [], 0)
+
